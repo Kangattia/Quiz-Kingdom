00cdafeb0140e1984ac5e82f38b1fa8a9fb2955a
Kangattia <joelkangattia@gmail.com>
2026-07-13 05:50:17 +0100

    Create sounds.js

---
+++ sounds.js
@@ -0,0 +1,11 @@
+const correctSound = new Audio(
+"https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg"
+);
+
+const wrongSound = new Audio(
+"https://actions.google.com/sounds/v1/cartoon/cartoon_boing.ogg"
+);
+
+const clickSound = new Audio(
+"https://actions.google.com/sounds/v1/buttons/button_click.ogg"
+);
