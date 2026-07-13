665368a3297f6c8c2310710e47435663bfd07486
Kangattia <joelkangattia@gmail.com>
2026-07-13 03:44:16 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,24 @@
+const questions = [
+  {
+    category: "History",
+    question: "Who was the first President of Cameroon?",
+    answers: [
+      "Ahmadou Ahidjo",
+      "Paul Biya",
+      "Ruben Um Nyobè",
+      "John Ngu Foncha"
+    ],
+    correct: 0
+  },
+  {
+    category: "History",
+    question: "Which ancient civilization built the pyramids of Giza?",
+    answers: [
+      "Romans",
+      "Egyptians",
+      "Greeks",
+      "Persians"
+    ],
+    correct: 1
+  }
+];
