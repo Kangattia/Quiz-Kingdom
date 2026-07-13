2428614a5f72f981a441a63a09bec2c97659ece5
Kangattia <joelkangattia@gmail.com>
2026-07-13 03:07:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const playBtn = document.getElementById("playBtn");
+
+playBtn.addEventListener("click", function () {
+
+    alert("Welcome to Quiz Kingdom!\n\nChoose History or Bible to begin.\n\n(The category selection screen is coming in the next step.)");
+
+});
