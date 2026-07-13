0a612512c801f74789e34e727cc11abfa251025f
Kangattia <joelkangattia@gmail.com>
2026-07-13 03:45:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,35 @@ const playBtn = document.getElementById("playBtn");
 const homeScreen = document.getElementById("homeScreen");
 const quizScreen = document.getElementById("quizScreen");
 
+const questionText = document.getElementById("questionText");
+const categoryTitle = document.getElementById("categoryTitle");
+
+const answerButtons = document.querySelectorAll(".answerBtn");
+
+let currentQuestion = 0;
+
+
 playBtn.addEventListener("click", function () {
+
     homeScreen.style.display = "none";
     quizScreen.style.display = "block";
+
+    showQuestion();
+
 });
+
+
+function showQuestion() {
+
+    let question = questions[currentQuestion];
+
+    categoryTitle.textContent = question.category;
+
+    questionText.textContent = question.question;
+
+    answerButtons[0].textContent = question.answers[0];
+    answerButtons[1].textContent = question.answers[1];
+    answerButtons[2].textContent = question.answers[2];
+    answerButtons[3].textContent = question.answers[3];
+
+}
