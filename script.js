48f3afb2d5a747309165943a8aae072b8d924f5b
Kangattia <joelkangattia@gmail.com>
2026-07-13 03:35:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ playBtn.addEventListener("click", function () {
     alert("Welcome to Quiz Kingdom!\n\nChoose History or Bible to begin.\n\n(The category selection screen is coming in the next step.)");
 
 });
+const playBtn = document.querySelector("#playBtn");
+const homeScreen = document.querySelector("#homeScreen");
+const quizScreen = document.querySelector("#quizScreen");
+
+playBtn.addEventListener("click", () => {
+  homeScreen.style.display = "none";
+  quizScreen.style.display = "block";
+});
