34e42ed9efe65a4d08469e8011aff1adc1d12fd8
Kangattia <joelkangattia@gmail.com>
2026-07-13 09:56:24 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,112 +1,131 @@
-const questions = [
-  {
-    category: "History",
-    question: "Who was the first President of Cameroon?",
-    answers: [
-      "Ahmadou Ahidjo",
-      "Paul Biya",
-      "Ruben Um Nyobè",
-      "John Ngu Foncha"
-    ],
-    correct: 0
-  },
-  {
-    category: "History",
-    question: "Which ancient civilization built the pyramids of Giza?",
-    answers: [
-      "Romans",
-      "Egyptians",
-      "Greeks",
-      "Persians"
-    ],
-    correct: 1
-  },
-  {
-    category: "History",
-    question: "Who was the first emperor of Rome?",
-    answers: [
-      "Julius Caesar",
-      "Augustus",
-      "Nero",
-      "Constantine"
-    ],
-    correct: 1
-  },
-  {
-    category: "History",
-    question: "Which country gifted the Statue of Liberty to the United States?",
-    answers: [
-      "France",
-      "Spain",
-      "Italy",
-      "Germany"
-    ],
-    correct: 0
-  },
-  {
-    category: "History",
-    question: "Who led India's independence movement through nonviolent resistance?",
-    answers: [
-      "Nelson Mandela",
-      "Mahatma Gandhi",
-      "Winston Churchill",
-      "Napoleon"
-    ],
-    correct: 1
-  },
-  {
-    category: "History",
-    question: "The Great Wall is located in which country?",
-    answers: [
-      "Japan",
-      "China",
-      "Korea",
-      "Mongolia"
-    ],
-    correct: 1
-  },
-  {
-    category: "History",
-    question: "Who was the first President of the United States?",
-    answers: [
-      "Abraham Lincoln",
-      "George Washington",
-      "Thomas Jefferson",
-      "John Adams"
-    ],
-    correct: 1
-  },
-  {
-    category: "History",
-    question: "Which empire was ruled by Mansa Musa?",
-    answers: [
-      "Roman Empire",
-      "Mali Empire",
-      "Ottoman Empire",
-      "British Empire"
-    ],
-    correct: 1
-  },
-  {
-    category: "History",
-    question: "In which year did World War II end?",
-    answers: [
-      "1945",
-      "1939",
-      "1950",
-      "1918"
-    ],
-    correct: 0
-  },
-  {
-    category: "History",
-    question: "Who was known as the founder of the Mongol Empire?",
-    answers: [
-      "Genghis Khan",
-      "Alexander the Great",
-      "Julius Caesar",
-      "Napoleon"
-    ],
-    correct: 0
-  }
-];
+const questionBank = {
+
+History: [
+
+{
+category: "History",
+question: "Who was the first President of Cameroon?",
+answers: [
+"Ahmadou Ahidjo",
+"Paul Biya",
+"Ruben Um Nyobè",
+"John Ngu Foncha"
+],
+correct: 0
+},
+
+{
+category: "History",
+question: "Which ancient civilization built the pyramids of Giza?",
+answers: [
+"Romans",
+"Egyptians",
+"Greeks",
+"Persians"
+],
+correct: 1
+},
+
+{
+category: "History",
+question: "Who was the first emperor of Rome?",
+answers: [
+"Julius Caesar",
+"Augustus",
+"Nero",
+"Constantine"
+],
+correct: 1
+},
+
+{
+category: "History",
+question: "Which country gifted the Statue of Liberty to the United States?",
+answers: [
+"France",
+"Spain",
+"Italy",
+"Germany"
+],
+correct: 0
+},
+
+{
+category: "History",
+question: "Who led India's independence movement through nonviolent resistance?",
+answers: [
+"Nelson Mandela",
+"Mahatma Gandhi",
+"Winston Churchill",
+"Napoleon"
+],
+correct: 1
+},
+
+{
+category: "History",
+question: "The Great Wall is located in which country?",
+answers: [
+"Japan",
+"China",
+"Korea",
+"Mongolia"
+],
+correct: 1
+},
+
+{
+category: "History",
+question: "Who was the first President of the United States?",
+answers: [
+"Abraham Lincoln",
+"George Washington",
+"Thomas Jefferson",
+"John Adams"
+],
+correct: 1
+},
+
+{
+category: "History",
+question: "Which empire was ruled by Mansa Musa?",
+answers: [
+"Roman Empire",
+"Mali Empire",
+"Ottoman Empire",
+"British Empire"
+],
+correct: 1
+},
+
+{
+category: "History",
+question: "In which year did World War II end?",
+answers: [
+"1945",
+"1939",
+"1950",
+"1918"
+],
+correct: 0
+},
+
+{
+category: "History",
+question: "Who was known as the founder of the Mongol Empire?",
+answers: [
+"Genghis Khan",
+"Alexander the Great",
+"Julius Caesar",
+"Napoleon"
+],
+correct: 0
+}
+
+],
+
+Bible: [
+
+]
+
+};
